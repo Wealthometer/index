2fe912621e4cfe50607dd7b75665b95a25e0955d
Wealthometer <wealthhajoh87@gmail.com>
2026-03-01 00:20:57 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,3 +15,4 @@ const register = async (req, res, next) => {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ success: false, message: 'User with this email already exists' });
+    }
