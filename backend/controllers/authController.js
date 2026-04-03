3f8c9e59464c43c8e138f31c4d2b1fab5f7109c0
Wealthometer <wealthhajoh87@gmail.com>
2026-04-04 00:14:09 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,3 +9,4 @@ const register = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
+      return res.status(400).json({ success: false, message: 'Please provide name, email and password' });
