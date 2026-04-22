e1295fcd623d0058f43e49fc2d4739ec59b97cde
Wealthometer <wealthhajoh87@gmail.com>
2026-04-23 00:06:05 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,3 +4,4 @@ const generateToken = require('../utils/generateToken');
 // @desc    Register a new user
 // @route   POST /api/auth/register
 // @access  Public
+const register = async (req, res, next) => {
