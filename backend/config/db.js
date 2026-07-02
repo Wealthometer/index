cdc56b140c4faced9f82f46d2077d0f3ace4a0cb
Wealthometer <wealthhajoh87@gmail.com>
2026-07-02 23:58:13 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,3 +5,4 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
+    console.error(`MongoDB connection error: ${err.message}`);
